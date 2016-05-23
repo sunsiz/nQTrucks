f4b1371a25cbed69713e3870a96df716b1ff8103
Newsages <newsages@hotmail.com>
2016-05-23 19:49:51 +0200

    Init Daemon

--- nqtrucks/Core/Daemon.cpp
+++ nqtrucks/Core/Daemon.cpp
@@ -1,7 +1,15 @@
 #include "Daemon.h"
 
-Daemon::Daemon()
+Daemon::Daemon(QObject *parent)
+    : QObject(parent)
+    , m_iniciado(false)
 {
 
+
+}
+
+void Daemon::setIniciado(bool iniciado)
+{
+    m_iniciado = iniciado;
 }
 
